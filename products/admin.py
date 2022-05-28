ae1ed1bf320836faa9f6d4d6de4f6f22442e4d65
liaz98 <azibonu19@gmail.com>
2022-05-29 00:13:14 +0500

    updated

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import Products, ProductRate, Category
+
+admin.site.register(Products)
+admin.site.register(ProductRate)
+admin.site.register(Category)
+
--- products/views.py
+++ products/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views import View
+from django.views.generic import TemplateView, DetailView, ListView
+
 
 # Create your views here.
+from products.models import Products, Category
+from shop.models import Inventory
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Inventory
+    context_object_name = 'inventories'
     template_name = 'pages/index.html'
 
 
-class ProductDetailView(TemplateView):
+class ProductDetailView(DetailView):
+    model = Products
+    context_object_name = 'product'
     template_name = 'pages/detail.html'
-
 
 class CategoryView(TemplateView):
     template_name = 'pages/category.html'
